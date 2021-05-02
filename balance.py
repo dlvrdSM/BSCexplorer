292207a2d73a658139cc0c4d124c370ad9f530f5
dlvrdSM <83557047+dlvrdSM@users.noreply.github.com>
2021-05-03 00:03:55 +0200

    Update balance.py

--- balance.py
+++ balance.py
@@ -4,8 +4,7 @@ def token_balance(contract_address, wallet_address):
     '''Get the token balance for a certain address.'''
     response = requests.get('https://api.bscscan.com/api?module=account&action=tokenbalance'
                            f'&contractaddress={contract_address}&address={wallet_address}&tag=latest&apikey={api_key}')
-    # extract the balance string from the dictionary
     result = response.json()['result']
-    # convert the result string to an integer and show it with che correct unit
-    balance = int(result) / pow(10, 9)
-    return balance
+    token_balance = int(result) / pow(10, 9) 
+    
+    return token_balance
